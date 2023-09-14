e119df60c9977d03340516f5feccc1664fca68b5
Gukin Han <gukin.dev@gmail.com>
2023-09-14 15:36:36 +0900

    충돌 해결

--- frontend/src/pages/IDEPage/components/header/index.jsx
+++ frontend/src/pages/IDEPage/components/header/index.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from "react";
+import Button from "./components/Button";
 
 const index = () => {
   return (
-    <header>
-        <nav>
-            <h1 className="text-center font-mono font-extrabold text-[50px] text-blue-500">this is the header. update</h1>
-        </nav>
+    <header className="flex justify-between py-2 px-4 bg-[#0F1524]">
+      <div className="flex w-1/3">
+        <div className=" bg-black text-white">userA</div>
+      </div>
+      <div className="flex w-1/5 justify-center">
+        <button className="bg-[#1E4919] hover:bg-green-300 rounded-md py-1 px-3 text-[#CCFDCE]">
+          Run
+        </button>
+      </div>
+      <div className="flex w-1/3 justify-end gap-6">
+        <Button name="+Invite" />
+        <Button name="+Fork" />
+        <Button name="+Share" />
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default index
+export default index;
