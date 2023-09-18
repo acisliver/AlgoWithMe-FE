689a0c4ec323f6c0289c03ddc9377b53d64fb246
[Minyoung <[leemy2200@gmail.com]>
2023-09-18 17:39:56 +0900

    파일 생성 기능 추가

--- frontend/src/pages/IDEPage/components/file_explorer/index.jsx
+++ frontend/src/pages/IDEPage/components/file_explorer/index.jsx
@@ -26,6 +26,7 @@ export default function Explorer({ selectedTab }) {
     },
       ]);
   const [selectedFolderKey, setSelectedFolderKey] = useState(null);
+  const [creatingItemType, setCreatingItemType] = useState(null);
 
 
   const searchChange = (e) => {
@@ -103,55 +104,60 @@ const toggleinput =() =>{
       }
       toggleinput();
 }
+
   
-// const createFolder = (folderName) => {
-//   const newFolder = {
-//     key: `${selectedFolderKey}-${Date.now()}`,
-//     title: folderName,
-//     type: 'folder',
-//     children: []
-//   };
+const createFolder = (folderName) => {
+  const newFolder = {
+    key: selectedFolderKey ? `${selectedFolderKey}-${Date.now()}` : `${Date.now()}`,
+    title: folderName,
+    type: 'folder',
+    children: []
+  };
 
-//   console.log("Creating folder:", newFolder); 
-
-
-//   if (!selectedFolderKey || !tree.find(node => node.key === selectedFolderKey)) {
-//     setTree(prevData => [...prevData, newFolder]);
-//   } else {
-//     setTree(prevData => 
-//       prevData.map(node => 
-//         node.key === selectedFolderKey ? 
-//           { ...node, children: [...node.children, newFolder] } : 
-//           node
-//       )
-//     );
-//   }
-// }
+  console.log("Creating folder:", newFolder); 
+  
+  if (!selectedFolderKey) {
+    setTree(prevData => [...prevData, newFolder]);
+  } else {
+    setTree(prevData => 
+      prevData.map(node => 
+        node.key === selectedFolderKey ? 
+          { ...node, children: [...node.children, newFolder] } : 
+          node
+      )
+    );
+  }
+  toggleinput();
+}
 
 
   const treeSubmit =(e) =>{
     console.log("treeSubmit called");
     e.preventDefault();
-      createFile(value)
+    if (creatingItemType === 'file') {
+      createFile(value);
+    } else if (creatingItemType === 'folder') {
+      createFolder(value);
+    }
     setValue("")
   }
   
 
   if(selectedTab === 'tabFiles'){
   return (
-    <div style={{maxWidth : '180px'}}>
+    <div style={{width : '168px'}}>
       <form className=' bg-[#0E1525]'>
-        <input className='bg-[#1D2332] hover:bg-[#2B3245] h-8 rounded-md  my-0.5 placeholder:pl-2 text-white' placeholder='Search' value={searchValue} onChange={searchChange} />
+        <input className='bg-[#1D2332] hover:bg-[#2B3245] h-8 rounded-md  my-0.5 placeholder:pl-2 text-white' style={{width: '160px'}} placeholder='Search' value={searchValue} onChange={searchChange} />
       </form>
       <div className='bg-[#0E1525] text-white flex justify-between font-medium pl-1 pr-2 pt-1'>
         Files
         <div className='flex '>
-          <button className='hover:bg-[#1C2333] rounded-lg' onClick={toggleinput} >
+          <button className='hover:bg-[#1C2333] rounded-lg' onClick={()=>{toggleinput();setCreatingItemType('file');}} >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mx-1 ">
         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
 </svg>
 </button>
-<button className='hover:bg-[#1C2333] rounded-lg' onClick={toggleinput}  >
+<button className='hover:bg-[#1C2333] rounded-lg'onClick={() => {toggleinput();setCreatingItemType('folder');}}  >
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mx-1">
   <path strokeLinecap="round" strokeLinejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
 </svg>
