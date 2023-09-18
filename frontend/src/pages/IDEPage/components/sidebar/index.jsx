45cd009543f7a7f9e95745bb75569b77882aee2f
[Minyoung <[leemy2200@gmail.com]>
2023-09-18 11:28:28 +0900

    토글기능 커밋

--- frontend/src/pages/IDEPage/components/sidebar/index.jsx
+++ frontend/src/pages/IDEPage/components/sidebar/index.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+import Explorer from "../file_explorer/index";
+
+
 const index = () => {
+  const [selectedTab, setSelectedTab] = useState("tabFiles");
+
+  const tabHandleClick = (tabName) => {
+    if (selectedTab === tabName) {
+      setSelectedTab("");
+    } else {
+      setSelectedTab(tabName);
+    }
+  }
+  
   return (
     <>
-    <div className="w-14 h-screen bg-[#0E1525] z-10 flex items-center flex-col fixed justify-between">
+    <div className="w-14 h-screen bg-[#0E1525] z-10 flex items-center flex-col fixed ">
       <div>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#666B75" className="w-7 h-7 hover:stroke-white">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-</svg>
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#666B75" className="w-7 h-7 hover:stroke-white">
   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
 </svg>
@@ -20,7 +31,7 @@ const index = () => {
 </svg>
 </div>
     </div>
-      
+    <Explorer selectedTab={selectedTab} />
     </>
   )
 }
