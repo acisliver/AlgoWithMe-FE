dbf5bc48d9daad8872419cfd324097492fd86ee0
hmlee0228 <leehymy98@naver.com>
2023-09-22 06:04:28 +0900

    에디터 UI수정,콘솔 닫기버튼 활성화

--- frontend/src/pages/IDEPage/components/console/components/CloseButton.jsx
+++ frontend/src/pages/IDEPage/components/console/components/CloseButton.jsx
@@ -1,12 +1,14 @@
+import React from 'react';
 import CloseIcon from "@mui/icons-material/Close";
 
-const CloseButton = () => {
+const CloseButton = ({ onClose }) => {
   return (
     <button
       type="button"
       id="south_tab_close_btn"
       className="btn"
       data-container="body"
+      onClick={onClose} // 클릭 이벤트 핸들러 추가
     >
       <CloseIcon />
     </button>
@@ -14,3 +16,4 @@ const CloseButton = () => {
 };
 
 export default CloseButton;
+
