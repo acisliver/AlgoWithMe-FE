74a5ed59cf93ef20d952b8b3c8873aa2905d934c
Gukin Han <gukin.dev@gmail.com>
2023-09-25 09:29:32 +0900

    design: run 버튼 마진 수정 및 색 조정

--- frontend/src/pages/IDEPage/components/header/components/ExeButton.jsx
+++ frontend/src/pages/IDEPage/components/header/components/ExeButton.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const ExeButton = (props) => {
   return (
     <button
-      className={`rounded-md py-1 px-3 ${
-        props.running ? "bg-[#2C3243] text-[#C3C8CC]" : "bg-[#1E4919] text-[#CCFDCE]"
-      }`} 
+      className={`rounded-md py-1 px-5 ${
+        props.running
+          ? "bg-[#2C3243] text-[#C3C8CC]"
+          : "bg-[#349B44] hover:bg-[#4cde62] text-[#f4f4f4] hover:text-[#f2f9f2]"
+      }`}
       onClick={props.toggleRunning}
     >
       {props.running ? "Stop" : "Run"}
