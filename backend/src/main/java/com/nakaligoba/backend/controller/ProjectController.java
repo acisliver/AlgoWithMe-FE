1ef8a3b8c1c8e212e4968f974777ffa4d045d248
Gyujin Kim <kjkj268@naver.com>
2023-09-25 16:54:26 +0900

    프로젝트 파일 생성 및 목록 불러오기 API 작성

---
+++ backend/src/main/java/com/nakaligoba/backend/controller/ProjectController.java
@@ -0,0 +1,31 @@
+package com.nakaligoba.backend.controller;
+
+import com.nakaligoba.backend.service.ProjectService;
+import com.nakaligoba.backend.service.ProjectService.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/projects")
+public class ProjectController {
+
+    private final ProjectService projectService;
+
+
+    @PostMapping
+    public ResponseEntity<ProjectCreateResponse> createProject(@Valid @RequestBody ProjectCreateRequest request) {
+        ProjectCreateResponse response = projectService.create(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllProjects() {
+        List<ProjectListResponse> projects = projectService.getAllProjects();
+        return ResponseEntity.ok(projects);
+    }
+}
---
+++ backend/src/main/java/com/nakaligoba/backend/repository/ProjectRepository.java
@@ -0,0 +1,8 @@
+package com.nakaligoba.backend.repository;
+
+import com.nakaligoba.backend.entity.ProjectEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
+    boolean existsByProjectName(String projectName);
+}
