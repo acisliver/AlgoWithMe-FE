28a3d41d7040f60d119a7545ebe96f7623a8076f
김규진 <67509973+gyujin-Kimm@users.noreply.github.com>
2023-09-25 19:09:32 +0900

    Controller 단에 service DTO 이동

--- backend/src/main/java/com/nakaligoba/backend/controller/ProjectController.java
+++ backend/src/main/java/com/nakaligoba/backend/controller/ProjectController.java
@@ -1,12 +1,12 @@
 package com.nakaligoba.backend.controller;
 
 import com.nakaligoba.backend.service.ProjectService;
-import com.nakaligoba.backend.service.ProjectService.*;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -16,7 +16,6 @@ public class ProjectController {
 
     private final ProjectService projectService;
 
-
     @PostMapping
     public ResponseEntity<ProjectCreateResponse> createProject(@Valid @RequestBody ProjectCreateRequest request) {
         ProjectCreateResponse response = projectService.create(request);
@@ -24,8 +23,41 @@ public class ProjectController {
     }
 
     @GetMapping
-    public ResponseEntity<ProjectListResponse> getAllProjects() {
+    public ResponseEntity<List<ProjectListResponse>> getAllProjects() {
         List<ProjectListResponse> projects = projectService.getAllProjects();
         return ResponseEntity.ok(projects);
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ProjectCreateRequest {
+        private String name;
+        private String description;
+        private String template;
+    }
+
+    @Data
+    @Builder
+    public static class ProjectCreateResponse {
+        private Long id;
+        private String storageId;
+    }
+
+    @Data
+    @Builder
+    public static class ProjectListResponse {
+        private String name;
+        private String description;
+        private LocalDateTime updatedAt;
+        private List<CollaboratorResponse> collaborators;
+    }
+
+    @Data
+    @Builder
+    public static class CollaboratorResponse {
+        private Long id;
+        private String name;
     }
 }
