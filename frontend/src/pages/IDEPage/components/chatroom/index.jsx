2505cbd2f234a25f373833d7d95a4e7e01ba5b32
Gukin Han <gukin.dev@gmail.com>
2023-09-25 16:30:50 +0900

    feat: 채팅창에 메시지 추가 기능 및 더미 데이터

--- frontend/src/pages/IDEPage/components/chatroom/index.jsx
+++ frontend/src/pages/IDEPage/components/chatroom/index.jsx
@@ -1,41 +1,92 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import ChatInput from "./component/ChatInput";
+import Message from "./component/Message";
+
+const data = {
+  projectId: 123,
+  messageHistory: [
+    {
+      messageId: 1,
+      memberId: 1,
+      memberName: "John Doe",
+      messageType: "ENTER",
+      createdAt: "2023-09-22T23:34:00",
+      systemMessage: "John Doe님이 입장했습니다.",
+    },
+    {
+      messageId: 2,
+      memberId: 1,
+      memberName: "John Doe",
+      messageType: "MESSAGE",
+      createdAt: "2023-09-22T23:35:00",
+      message: "Hello, how are you?",
+    },
+    {
+      messageId: 3,
+      memberId: 2,
+      memberName: "Jane Doe",
+      messageType: "MESSAGE",
+      createdAt: "2023-09-22T23:36:00",
+      message: "I'm good, thank you!",
+    },
+    {
+      messageId: 4,
+      memberId: 1,
+      memberName: "John Doe",
+      messageType: "EXIT",
+      createdAt: "2023-09-22T23:37:00",
+      systemMessage: "John Doe님이 나갔습니다.",
+    },
+  ],
+};
 
 const index = (props) => {
   const [openChatroom, setOpenChatroom] = useState(false);
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState(data.messageHistory);
+  const messageListRef = useRef(null); // Create a ref
 
   const clickHandler = () => setOpenChatroom((prev) => !prev);
-  const changeHandler = (event) => {
-    setMessage(event.target.value)
-  }
 
+  const enterHandler = (newMessage) => {
+    const messageForm = {
+      messageId: 3,
+      memberId: 2,
+      memberName: "Jane Doe",
+      messageType: "MESSAGE",
+      createdAt: "2023-09-22T23:36:00",
+      message: newMessage,
+    };
+
+    setMessages((prev) => [...prev, messageForm]);
+  };
+
+  useEffect(() => {
+    if (messageListRef.current) {
+      messageListRef.current.scrollTop = messageListRef.current.scrollHeight; // Scroll to bottom
+    }
+  }, [messages]); // Trigger whenever messages array updates
 
   return (
     <div className="relative">
       {openChatroom && (
-        <div className="absolute bottom-24 right-10 bg-white rounded-xl p-2 w-[350px] h-[500px] shadow-md flex flex-col justify-between">
-          <div className="flex-none">This is top</div>
-          <div className="flex-grow">This is middle</div>
-          <div className="flex-none flex items-center relative">
-            <input
-              type="text"
-              placeholder="Enter messages"
-              className="py-1 px-2 w-full bg-[#e2e2e2] rounded-xl flex-none"
-              onChange={changeHandler}
-              value={message}
-            ></input>
-            {message.length > 0 && <button
-              type="button"
-              className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-transparent text-black rounded-lg p-1"
-              onClick={() => {
-                /* Handle send message */
-              }}
-            >
-              Send
-            </button>}
-            
+        <div className="absolute bottom-24 right-10 bg-[#2B3244] border-2 border-[#8E9DC7] rounded-xl p-2 w-[400px] h-[500px] shadow-md flex flex-col justify-between">
+          <div className="flex-none text-center text-white">This is top</div>
+          <div className="flex-grow overflow-auto" ref={messageListRef}>
+            <ul className="message-list p-0 m-0">
+              {messages.map((message, index) => (
+                <li key={message.messageId || index} className="list-none mb-2">
+                  <Message
+                    messageType={message.messageType}
+                    memberName={message.memberName}
+                    message={message.message}
+                    systemMessage={message.systemMessage}
+                    createdAt={message.createdAt}
+                  />
+                </li>
+              ))}
+            </ul>
           </div>
+          <ChatInput onEnter={enterHandler} />
         </div>
       )}
       <button
