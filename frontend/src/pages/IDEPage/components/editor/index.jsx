71221a624865521400c48a81a689c576ef89de40
hmlee0228 <leehymy98@naver.com>
2023-09-26 07:49:40 +0900

    CodeMirror 적용(탭 유형에 따른 에디터 설정)

--- frontend/src/pages/IDEPage/components/editor/index.jsx
+++ frontend/src/pages/IDEPage/components/editor/index.jsx
@@ -1,41 +1,62 @@
-
 import React, { useState, useRef } from "react";
 import Tab from "./components/Tab";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { java } from "@codemirror/lang-java";
+import { html } from "@codemirror/lang-html";
+import { css } from "@codemirror/lang-css";
+import { tokyoNight } from "@uiw/codemirror-theme-tokyo-night";
 import "./index.css";
 
 //탭
 const Index = () => {
   const [activeTab, setActiveTab] = useState("Main.java");
   const [tabs, setTabs] = useState([
-    "Main.java",
-    "index.jsx",
-    "index.css",
-    "index1.jsx",
-    "index2.jsx",
-    "index3.jsx",
-    "index4.jsx",
-    "index5.jsx",
-    "index6.jsx",
-    "index7.jsx",
-    "index8.jsx",
-    "index9.jsx",
-    "index0.jsx",
+    {title: "Main.java", content: "console.log('Hello World!'); 111 abcd"},
+    {title: "index.jsx", content: "console.log('Hello World!'); 111 abcd"},
+    {title: "index.css", content: "222222" },
+    {title: "index1.jsx", content: "3" },
+    {title: "index2.jsx", content: "4" },
+    {title: "index3.jsx", content: "5" },
+    {title: "index4.jsx", content: "6" },
+    {title: "index5.jsx", content: "7" },
+    {title: "index6.jsx", content: "8" },
+    {title: "index7.jsx", content: "9" },
+    {title: "index8.jsx", content: "10" },
+    {title: "index9.jsx", content: "11111" },
+    {title: "index0.jsx", content: "1222222" },
   ]);
 
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
+  const handleTabClick = (tabTitle) => {
+    setActiveTab(tabTitle);
+    const newTabContent = tabs.find(tab => tab.title === tabTitle)?.content || "";
+    setValue(newTabContent);
   };
 
+  //탭 유형에 따른 에디터 설정
+  const getExtensionForTab = (tabTitle) => {
+    if (tabTitle.endsWith(".java")) {
+      return java();
+    } else if (tabTitle.endsWith(".html")) {
+      return html();
+    } else if (tabTitle.endsWith(".jsx") || tabTitle.endsWith(".js")) {
+      return javascript({ jsx: true });
+    } else if (tabTitle.endsWith(".css")) {
+      return css();
+    }
+  };
+  const activeExtension = getExtensionForTab(activeTab);
+  
 
   //닫기 버튼
-  const handleCloseTab = (tabToClose) => {
-    setTabs(tabs.filter((tab) => tab !== tabToClose));
-    if (activeTab === tabToClose) {
-      setActiveTab(tabs[0]);
+  const handleCloseTab = (tabTitleToClose) => {
+    setTabs(tabs.filter((tab) => tab.title !== tabTitleToClose));
+    if (activeTab === tabTitleToClose && tabs.length > 0) {
+      setActiveTab(tabs[0].title);
     }
   };
 
-   //스크롤
+  //스크롤
   const tabsRef = useRef(null);
   const handleWheel = (e) => {
     e.stopPropagation();
@@ -52,6 +73,21 @@ const Index = () => {
     document.body.classList.remove("no-scroll");
   };
 
+  // CodeMirror 적용
+  const activeTabContent = tabs.find(tab => tab.title === activeTab)?.content || "";
+  const [value, setValue] = React.useState(activeTabContent);
+  
+  const onChange = React.useCallback((val, viewUpdate) => {
+    console.log("val:", val);
+    setTabs(tabs.map(tab => {
+      if (tab.title === activeTab) {
+        return { ...tab, content: val };
+      }
+      return tab;
+    }));
+    setValue(val);
+  }, [activeTab, tabs]);
+
   return (
     <div id="inner_layout_top" className="bg-[#0E1525] overflow-hidden">
       <ul
@@ -67,14 +103,23 @@ const Index = () => {
         {tabs.map((tab, index) => (
           <Tab
             key={index}
-            isActive={activeTab === tab}
-            label={tab}
-            onClick={() => handleTabClick(tab)}
-            onClose={() => handleCloseTab(tab)}
+            isActive={activeTab === tab.title}
+            label={tab.title}
+            onClick={() => handleTabClick(tab.title)}
+            onClose={() => handleCloseTab(tab.title)}
           />
         ))}
       </ul>
-      <div className="text-white">{activeTab} 내용 </div>
+      <div className="text-gray-400">
+        <CodeMirror
+          key={activeTab}
+          value={value}
+          height="56vh;" // #inner_layout_top 가 60vh로 설정되어있기때문
+          extensions={[activeExtension]}
+          onChange={onChange}
+          theme={tokyoNight} // 테마 
+        />
+      </div>
     </div>
   );
 };
