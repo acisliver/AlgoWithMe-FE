464deb07579c85fedc34031bae24247c32dc2757
Seokjon Son <tnh3113@naver.com>
2023-09-28 23:24:58 +0900

    feat: 이메일 인증을 위한 인증번호 전송(#83)

--- backend/src/main/java/com/nakaligoba/backend/controller/MemberController.java
+++ backend/src/main/java/com/nakaligoba/backend/controller/MemberController.java
@@ -1,10 +1,8 @@
 package com.nakaligoba.backend.controller;
 
 import com.nakaligoba.backend.service.MemberService;
-import com.nakaligoba.backend.service.MemberService.MemberDto;
 import com.nakaligoba.backend.utils.JwtUtils;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 
 @Slf4j
@@ -24,7 +23,7 @@ public class MemberController {
 
     // 회원가입
     @PostMapping("/signup")
-    public ResponseEntity<SignupResponse> signup(@RequestBody SignupRequest request) {
+    public ResponseEntity<SignupResponse> signup(@Valid @RequestBody SignupRequest request) {
         MemberDto memberDto = MemberDto.builder()
                 .email(request.getEmail())
                 .password(request.getPassword())
@@ -39,7 +38,7 @@ public class MemberController {
 
     // 로그인
     @PostMapping("/signin")
-    public ResponseEntity<SigninResponse> signin(@RequestBody SigninRequest request) {
+    public ResponseEntity<SigninResponse> signin(@Valid @RequestBody SigninRequest request) {
         MemberDto memberDto = MemberDto.builder()
                 .email(request.getEmail())
                 .password(request.getPassword())
@@ -49,6 +48,17 @@ public class MemberController {
         String jwt = memberService.signin(memberDto);
 
         return ResponseEntity.ok(new SigninResponse(JwtUtils.BEARER + jwt, "정상적으로 로그인 되었습니다."));
+    }
+
+    @PostMapping("/email")
+    public ResponseEntity<EmailAuthResponse> authEmail(@Valid @RequestBody EmailAuthRequest request) {
+        AuthEmailDto authEmailDto = AuthEmailDto.builder()
+                .email(request.email)
+                .build();
+
+        memberService.authEmail(authEmailDto);
+
+        return ResponseEntity.ok(new EmailAuthResponse("인증번호를 전송하였습니다."));
     }
 
     @Data
@@ -81,5 +91,35 @@ public class MemberController {
     static class SigninResponse {
         private final String accessToken;
         private final String message;
+    }
+
+    @Data
+    @NoArgsConstructor
+    static class EmailAuthRequest {
+        @NotBlank
+        private String email;
+    }
+
+    @Data
+    static class EmailAuthResponse {
+        private final String message;
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class MemberDto {
+        private String email;
+        private String password;
+        private String name;
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class AuthEmailDto {
+        private String email;
     }
 }
--- backend/src/main/java/com/nakaligoba/backend/service/MemberService.java
+++ backend/src/main/java/com/nakaligoba/backend/service/MemberService.java
@@ -1,9 +1,15 @@
 package com.nakaligoba.backend.service;
 
+import com.nakaligoba.backend.controller.MemberController.AuthEmailDto;
+import com.nakaligoba.backend.controller.MemberController.MemberDto;
 import com.nakaligoba.backend.entity.MemberEntity;
 import com.nakaligoba.backend.jwt.JwtProvider;
 import com.nakaligoba.backend.repository.MemberRepository;
-import lombok.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -12,20 +18,28 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.time.LocalDateTime;
+import java.util.Random;
 
+@PropertySource("classpath:application.yml")
 @RequiredArgsConstructor
 @Service
 public class MemberService {
+
+    @Value("${spring.mail.username}")
+    private String sender;
 
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder passwordEncoder;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final JwtProvider jwtProvider;
+    private final JavaMailSender javaMailSender;
 
     @Transactional
     public void signup(MemberDto memberDto) {
-        if (memberRepository.existsByEmail(memberDto.email)) {
+        if (memberRepository.existsByEmail(memberDto.getEmail())) {
             throw new IllegalArgumentException("이미 등록된 이메일입니다.");
         }
 
@@ -34,18 +48,16 @@ public class MemberService {
                 .password(passwordEncoder.encode(memberDto.getPassword()))
                 .name(memberDto.getName())
                 .build();
-        memberEntity.setCreatedAt(LocalDateTime.now());
-        memberEntity.setUpdatedAt(LocalDateTime.now());
 
         memberRepository.save(memberEntity);
     }
 
     @Transactional
     public String signin(MemberDto memberDto) {
-        if (memberRepository.existsByEmail(memberDto.email)) {
-            MemberEntity memberEntity = memberRepository.findByEmail(memberDto.email);
+        if (memberRepository.existsByEmail(memberDto.getEmail())) {
+            MemberEntity memberEntity = memberRepository.findByEmail(memberDto.getEmail());
 
-            if (passwordEncoder.matches(memberDto.password, memberEntity.getPassword())) {
+            if (passwordEncoder.matches(memberDto.getPassword(), memberEntity.getPassword())) {
                 UsernamePasswordAuthenticationToken authenticationToken
                         = new UsernamePasswordAuthenticationToken(memberDto.getEmail(), memberDto.getPassword());
                 Authentication authentication
@@ -61,13 +73,36 @@ public class MemberService {
         }
     }
 
-    @Data
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class MemberDto {
-        private String email;
-        private String password;
-        private String name;
+    @Transactional
+    public void authEmail(AuthEmailDto authEmailDto) {
+        String authNumber = getAuthNumber();
+        sendAuthEmail(authEmailDto, authNumber);
+    }
+
+    private String getAuthNumber() {
+        Random random = new Random();
+        return String.valueOf(111111 + random.nextInt(888889));
+    }
+
+    private void sendAuthEmail(AuthEmailDto authEmailDto, String authNumber) {
+        String title = "NakaLiGoBa 회원가입 인증 메일입니다.";
+        String contents = "";
+        contents += "안녕하세요. NakaLiGoBa 입니다.<br/>";
+        contents += "NakaLiGoBa 회원가입을 진심으로 환영합니다.<br/><br/>";
+        contents += "아래의 인증번호를 입력하여 인증을 하시면 NakaLiGoBa 회원가입이 완료됩니다.<br/><br/>";
+        contents += "회원가입 인증번호 : ";
+        contents += authNumber;
+
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "utf-8");
+            mimeMessageHelper.setFrom(sender);
+            mimeMessageHelper.setTo(authEmailDto.getEmail());
+            mimeMessageHelper.setSubject(title);
+            mimeMessageHelper.setText(contents, true);
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
