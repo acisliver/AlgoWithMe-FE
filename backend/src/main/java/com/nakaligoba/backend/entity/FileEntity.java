d4258ee4a765818ae71f5a8d6e2fdfbd04d48e5f
Daeeun Ko <7273322@naver.com>
2023-10-04 08:15:02 +0900

    fix: FileEntity ERD에 맞게 수정

--- backend/src/main/java/com/nakaligoba/backend/entity/FileEntity.java
+++ backend/src/main/java/com/nakaligoba/backend/entity/FileEntity.java
@@ -17,44 +17,17 @@ public class FileEntity extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
-    private String name;
-
-    // Todo storageFileId 필드 추가
     @Column(name = "storage_file_id", nullable = false)
     private String storageFileId;
 
-    @Column(name = "ext", nullable = false)
-    private String ext;
-
-    @Lob
-    @Column(name = "content", nullable = false)
-    private String content;
-
-    // Todo projectEntity 컬럼 추가
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "project_id", nullable = false)
     private ProjectEntity project;
 
     @Builder
-    public FileEntity(String name, String storageFileId, String ext, String content, ProjectEntity project) {
-        this.name = name;
+    public FileEntity(String storageFileId, ProjectEntity project) {
         this.storageFileId = storageFileId;
-        this.ext = ext;
-        this.content = content;
         this.project = project;
     }
 
-    public void changeName(String newName) {
-        this.name = newName;
-    }
-    public void changeExt(String newExt) {
-        this.ext = newExt;
-    }
-    public void changeContent(String newContent) {
-        this.content = newContent;
-    }
-    public void changeStorageFileId(String storageFileId) {
-        this.storageFileId = storageFileId;
-    }
 }
