767a5c86b2138367276f4fe80af7ca2b7e4b2821
[Minyoung <[leemy2200@gmail.com]>
2023-10-04 18:19:32 +0900

    api url 변경

--- frontend/src/service/apiUtils.js
+++ frontend/src/service/apiUtils.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import {API_BASE_URL} from "../config/api-config.js";
+// import {API_BASE_URL} from "../config/api-config.js";
 
 async function call(apiUrl, method, requestData = {}) {
     try {
         const response = await axios({
-            url: API_BASE_URL + apiUrl,
+            url: 'http://50.19.246.89:8080/api' + apiUrl,
             method,
             ...requestData
         });
