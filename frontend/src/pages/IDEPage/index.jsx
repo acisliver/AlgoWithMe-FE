ec6b1c71f008c68f6a23c1ffee8b1df12967dff2
hmlee0228 <leehymy98@naver.com>
2023-10-06 03:18:57 +0900

    로그인 시 user 데이터 전송

--- frontend/src/pages/IDEPage/index.jsx
+++ frontend/src/pages/IDEPage/index.jsx
@@ -5,36 +5,54 @@ import FileExplorer from "./components/file_explorer";
 import Header from "./components/header";
 import Sidebar from "./components/sidebar";
 import Modal from "./components/modal";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axiosInstance from "../../service/axiosInstance";
 
 const index = () => {
   const [onModalClick, setOnModalClick] = useState(true);
-  const [modal,setModal] =React.useState(false);
-
+  const [modal, setModal] = React.useState(false);
 
   const projectBtnHandler = (isOn) => {
     setOnModalClick(isOn);
   };
 
-  const createModal= ()=> {
-    setModal((prev)=>!prev);
-  }
+  const createModal = () => {
+    setModal((prev) => !prev);
+  };
 
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axiosInstance.get("user");
+        setUser(response.data);
+      } catch (error) {
+        console.error("Error fetching user data: ", error);
+      }
+    };
+    fetchUserData();
+  }, []);
 
   return (
     <div>
-    {onModalClick && <Modal 
-                        onProjClick={projectBtnHandler} 
-                        modal ={modal} 
-                        setModal={setModal} 
-                        createModal={createModal}
-                  
-                        />}
+      {onModalClick && (
+        <Modal
+          onProjClick={projectBtnHandler}
+          modal={modal}
+          setModal={setModal}
+          createModal={createModal}
+          user={user} // 사용자 데이터를 prop으로 전달
+        />
+      )}
       <div className="flex flex-col h-screen relative">
         <Header onProjClick={projectBtnHandler} />
         <div className="flex flex-row h-full">
           <div className="flex flex-row w-1/4">
-            <Sidebar createModal={createModal} projectBtnHandler={projectBtnHandler}/>
+            <Sidebar
+              createModal={createModal}
+              projectBtnHandler={projectBtnHandler}
+            />
             <FileExplorer />
           </div>
           <div className="flex flex-col w-3/4 h-full">
