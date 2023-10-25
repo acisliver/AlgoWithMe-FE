9059f70a4c23f4c425d70f71947b9e1c85849843
Gukin Han <gukin.dev@gmail.com>
2023-10-25 15:06:07 +0900

    Feat: 알고리즘 문제 페이지 UI 추가

--- src/components/pages/IDEPage/index.jsx
+++ src/components/pages/IDEPage/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Problems from '../../UI/molecules/Problems';
 
-const index = () => <div>This is IDEPage</div>;
+const index = () => (
+  <div>
+    <Problems />
+  </div>
+);
 
 export default index;
