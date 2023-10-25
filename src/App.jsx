9e433a57902a48eb28c4df0c2f3ced32dc8934cb
Gukin Han <gukin.dev@gmail.com>
2023-10-25 11:45:54 +0900

    Chore: eslint, prettier 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import React from 'react';
 
 function App() {
-
-
   return (
-    <>
- <div>
+    <div>
       <p className="text-red-400">Red Text</p>
       <p className="text-stone-900">Blue Text</p>
     </div>
-       
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
