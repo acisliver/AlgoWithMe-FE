b8caa5d97651f66627de53f6dc9f58818c55d8cf
hmlee0228 <leehymy98@naver.com>
2023-10-26 17:07:38 +0900

    route 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import AuthPage from './components/pages/AuthPage';
+import SigninPage from './components/pages/AuthPage/SigninPage';
+import SignupPage from './components/pages/AuthPage/SignupPage';
 import HomePage from './components/pages/HomePage';
 import IDEPage from './components/pages/IDEPage';
 
@@ -10,7 +11,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/signin" element={<SigninPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/problems" element={<IDEPage />} />
       </Routes>
     </BrowserRouter>
